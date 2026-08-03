cc3db287fc6684a264ed1b81145217a31e6b1e63
Prokop Ferdan <prokes1@gmail.com>
2026-08-03 11:13:51 +0200

    created entity ClassName

---
+++ src/main/java/com/prokys/teachers_journal/entity/ClassName.java
@@ -0,0 +1,20 @@
+package com.prokys.teachers_journal.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class ClassName {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+    private String name;
+
+}
