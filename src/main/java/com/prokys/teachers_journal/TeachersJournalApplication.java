34b2e8ad196f26eed4db75d7814de5ebab6fc330
Prokop Ferdan <prokes1@gmail.com>
2026-08-03 08:48:31 +0200

    starting project with spring initializer

---
+++ src/main/java/com/prokys/teachers_journal/TeachersJournalApplication.java
@@ -0,0 +1,13 @@
+package com.prokys.teachers_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachersJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachersJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prokys/teachers_journal/TeachersJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prokys.teachers_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachersJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
