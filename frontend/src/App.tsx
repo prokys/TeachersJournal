9cb1d2c9adfdbe309bb235bb9e677b0221176048
Prokop Ferdan <prokes1@gmail.com>
2026-08-12 11:16:58 +0200

    created SubjectsPage, added MainLayout and added Route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import {BrowserRouter, Navigate, Route, Routes} from "react-router";
 import TasksPage from "./pages/TasksPage.tsx"
 import CalendarPage from "./pages/CalendarPage.tsx"
 import LoginPage from "./pages/LoginPage.tsx"
+import SubjectsPage from "./pages/SubjectsPage.tsx";
+
 import './App.css'
 
 function App() {
@@ -20,6 +22,8 @@ function App() {
         <Route path="/calendar" element={<CalendarPage/>}/>
 
         <Route path="/login" element={<LoginPage/>}/>
+
+        <Route path="/subjects" element={<SubjectsPage/>}/>
 
     </Routes>
 
---
+++ frontend/src/pages/SubjectsPage.tsx
@@ -0,0 +1,19 @@
+import { Typography } from "@mui/material";
+import MainLayout from "../components/layouts/MainLayout";
+
+export default function SubjectsPage(){
+    return (
+        
+        <MainLayout>
+
+            <Typography
+                variant="h4"
+            >
+                Subjects
+            </Typography>
+
+
+        </MainLayout>
+    
+    );
+}
