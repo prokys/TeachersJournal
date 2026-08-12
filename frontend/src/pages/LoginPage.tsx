dcaad41c3a4576e3b28b409081b4c8bb83f91948
Prokop Ferdan <prokes1@gmail.com>
2026-08-12 11:11:31 +0200

    added MainLayout to CalendarPage

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,3 +1,62 @@
-export default function LoginPage(){
-    return <div></div>;
+import {
+    Box,
+    Button,
+    Paper,
+    TextField,
+    Typography,
+} from "@mui/material";
+
+export default function LoginPage() {
+    return (
+        <Box
+            sx={{
+                minHeight: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                bgcolor: "background.default",
+            }}
+        >
+
+            <Paper
+                elevation={3}
+                sx={{
+                    width: 400,
+                    p: 4,
+                }}
+            >
+
+                <Typography
+                    variant="h4"
+                >
+                    Teachers Journal
+                </Typography>
+
+                <TextField
+                    fullWidth
+                    label="Login"
+                    type="username"
+                    margin="normal"
+                />
+
+                <TextField
+                    fullWidth
+                    label="Password"
+                    type="password"
+                    margin="normal"
+                />
+
+                <Button
+                    fullWidth
+                    variant="contained"
+                    size="large"
+                    sx={{ mt: 2 }}
+                >
+                    Login
+                </Button>
+
+            </Paper>
+
+        </Box>
+    );
 }
