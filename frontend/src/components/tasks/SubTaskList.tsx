d7db69973a63c334f6ec78ed8e84c35388956b4b
Prokop Ferdan <prokes1@gmail.com>
2026-08-19 10:41:50 +0200

    updated SubTaskList to show data, update title

--- frontend/src/components/tasks/SubTaskList.tsx
+++ frontend/src/components/tasks/SubTaskList.tsx
@@ -1,3 +1,4 @@
+import { Box, Checkbox, Paper, Typography } from "@mui/material";
 import type { SubTask } from "../types/task";
 
 interface SubTaskListProps {
@@ -5,7 +6,64 @@ interface SubTaskListProps {
 }
 
 export default function SubTaskList ( {subtasks} : SubTaskListProps){
+    const completedCount = subtasks.filter(
+        (subtask) => subtask.completed
+    ).length;
+
     return (
-        <div></div>
-    )
+        <Paper
+            variant="outlined"
+            sx={{
+                p: 3,
+                borderRadius: 2,
+            }}
+        >
+            <Typography
+                variant="h6"
+            >
+                Subtasks ({completedCount}/{subtasks.length})
+            </Typography>
+
+            <Box sx={{ mt: 2 }}>
+                {subtasks.map((subtask) => (
+                    <Box
+                        key={subtask.id}
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <Checkbox
+                            checked={subtask.completed}
+                        />
+
+                        <Typography
+                            sx={{
+                                textDecoration:
+                                    subtask.completed
+                                        ? "line-through"
+                                        : "none",
+
+                                color:
+                                    subtask.completed
+                                        ? "text.secondary"
+                                        : "text.primary",
+                            }}
+                        >
+                            {subtask.title}
+                        </Typography>
+                    </Box>
+                ))}
+
+                {subtasks.length === 0 && (
+                    <Typography
+                        color="text.secondary"
+                        sx={{ mt: 2 }}
+                    >
+                        No subtasks yet.
+                    </Typography>
+                )}
+            </Box>
+        </Paper>
+    );
 }
