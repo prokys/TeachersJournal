e69be4b9b60b81061e430c614f7447cf3edf949b
Prokop Ferdan <prokes1@gmail.com>
2026-08-19 12:29:53 +0200

    updated NoteList to show data

--- frontend/src/components/tasks/NoteList.tsx
+++ frontend/src/components/tasks/NoteList.tsx
@@ -1,3 +1,4 @@
+import { Box, Divider, Paper, Typography } from "@mui/material";
 import type { Note } from "../types/task";
 
 interface NoteListProps {
@@ -6,6 +7,51 @@ interface NoteListProps {
 
 export default function NoteList ({ notes } : NoteListProps){
     return (
-        <div></div>
-    )
+        <Paper
+            variant="outlined"
+            sx={{
+                p: 3,
+                borderRadius: 2,
+            }}
+        >
+            <Typography
+                variant="h6"
+            >
+                Notes
+            </Typography>
+
+            <Box sx={{ mt: 2 }}>
+                {notes.map((note, index) => (
+                    <Box key={note.id}>
+                        {index > 0 && (
+                            <Divider sx={{ my: 2 }} />
+                        )}
+
+                        <Typography>
+                            {note.text}
+                        </Typography>
+
+                        <Typography
+                            variant="caption"
+                            color="text.secondary"
+                            sx={{
+                                display: "block",
+                                mt: 1,
+                            }}
+                        >
+                            {new Date(
+                                note.createdAt
+                            ).toLocaleString("cs-CZ")}
+                        </Typography>
+                    </Box>
+                ))}
+
+                {notes.length === 0 && (
+                    <Typography color="text.secondary">
+                        No notes yet.
+                    </Typography>
+                )}
+            </Box>
+        </Paper>
+    );
 }
